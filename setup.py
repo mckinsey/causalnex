cf74bda8adddb3e03efa2e2ff5517414afd64380
GabrielAz <gabriel.azevedoferreira@quantumblack.com>
2022-06-09 18:47:11 +0800

    updating Readme, release and docs files

--- setup.py
+++ setup.py
@@ -81,6 +81,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     extras_require=extras_require,
 )
