2f8a7b4e8ff1566ecee1f567acb749bb7ed08fba
tonyabracadabra <tongxupeng.cpu@gmail.com>
2022-09-06 01:15:46 +0800

    chore: update circleci

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     extras_require=extras_require,
 )
